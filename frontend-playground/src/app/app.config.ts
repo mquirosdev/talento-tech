62ba677b07b396726435da722bdc2a0ffbe50351
Monica Quiros <mquirosdev@gmail.com>
2026-02-15 14:46:14 -0500

    Add frontend

---
+++ frontend-playground/src/app/app.config.ts
@@ -0,0 +1,15 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter } from '@angular/router';
+/* llamado libreria para usar llamados http */
+import { provideHttpClient } from '@angular/common/http';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideHttpClient()
+    
+  ]
+};
---
+++ frontend-playground/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { UserComponent } from './view/user/user.component/user.component';
+
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'users',pathMatch:'full' },
+    { path: 'users', component: UserComponent}
+    /* { path: 'login', component: LoginComponent},
+    { path: 'register', component: RegisterComponent}, */
+];
